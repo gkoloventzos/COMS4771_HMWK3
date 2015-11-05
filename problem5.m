6ddbc325b1c4222f6390a144b21adb5e72ba62ba
Georgios Koloventzos <g.koloventzos@gmail.com>
2015-11-05 06:01:05 -0500

    gradient ready

--- problem5.m
+++ problem5.m
@@ -1,74 +1,115 @@
-function problem5()
-    close all ; 
-    load('data1.mat');
-    stepsize = 2;
-    tol = 1e-2;
-    lambda= 1e-2;
-    theta = rand(size(TestX, 2), 1 );
-    maxiter = 200000;
-    curiter = 0;
-    % For plotting stats
-    risks = [];
-    errs = [];
-    prevtheta = theta+2*tol;
-    while norm(theta - prevtheta) >= tol
-        if curiter > maxiter
-            break ;
-        end
-        % Current stats
-        kernel = rbf_kernel(TrainingX,TestX,size(TestX,1))
-        r = risk(TestX, TestY, theta );
-        f = 1./(1+exp(-TestX*theta));
-        f(f >= 0.5) = 1;
-        f(f < 0.5) = -1;
-        err = sum(f~=TestY)/length(TestY) ;
-        %fprintf ( 'Iter:%d , error:%0.4f , risk:%0.4f \n' , curiter , err , r );
-        %risks = cat(1, risks , r );
-        errs = cat(1, errs , err );
-        % Update theta
-        prevtheta = theta ;
-        G = gradient (TestX, TestY, theta, kernel);
-        theta = theta - stepsize*G;
-        curiter = curiter + 1;
-    end
-    %figure , plot (1: curiter , errs , 'r' , 1:curiter , risks , 'g' );
-    %title('Error ( red ) and risk ( green ) vs . iterations ' );
-    %disp('theta');
-    %disp(theta)
-    x=0:0.01:1;
-    y=(-theta(3) - theta (1).* x)/ theta (2);
-    %figure , plot (x, y, 'r' ); hold on ;
-    %plot (TestX(: , 1 ) , TestX(: , 2 ) , '.' ) ;
-    %title ( 'Linear decision boundary ' );
-end
+function problem5(stepsize)
 
-function R = risk (x, y, theta )
-    f = 1./(1+exp(-x* theta ));
-    r = (y-1).*log(1-f)-y.*log(f); r(isnan(r)) = 0;
-    R = mean(r);
-end
-    
-function g = gradient (x, y, theta, lambda)
-
-    g = -(1/size(x,1))*1-(1/1+exp(y*theta'*kernel)*y*kernel') + 2*lambda*theta';
+gradient_decent(stepsize)
 
 end
 
-%function th=myexp(x,theta)
-%    th=0;
+%function sgd(points)
+%
 %end
 
-function K = rbf_kernel(X,X2)
+function gradient_decent(stepsize)
+    close all ; 
+    load('data1.mat');
+    kernel = rbf_kernel(TrainingX);
+    tol = exp(-3);
+    lambda= exp(-2);
+    %prevtheta = 0.1 * (rand(size(TrainingX,1),1)-0.5);
+    prevtheta = ones(size(TrainingX,1),1);
+    curiter = 0;
+    %start time
+    time_start = tic;
+    all_time(1) = time_start;
+    cost(1) = get_cost(TrainingY,prevtheta,kernel,lambda);
+    S_G = ggradient(TrainingY, prevtheta, lambda, kernel);
+    theta = prevtheta - stepsize.*S_G;
+    diff = norm(theta - prevtheta);
+    while diff >= tol
+        % Update theta
+        prevtheta = theta;
+        G = ggradient(TrainingY, prevtheta, lambda, kernel);
+        theta = prevtheta - stepsize.*G;
+        time_now = toc(time_start);
+        diff = norm(theta -prevtheta);
+        curiter = curiter + 1;
+        all_time(curiter) = time_now
+        cost(curiter) = get_cost(TrainingY,prevtheta,kernel,lambda)
+        accuracy(curiter) = error(prevtheta,kernel,TestY);
+    end
+    fprintf ('Accuracy:%0.4f, step:%0.4f, iter:%d, total time: %d  \n' , accuracy(end), stepsize, curiter, all_time(end) );
+    figure
+    %index = find(all_time > 20,1)
+    plot(all_time(:), cost(:))
+    ylabel('Cost -J(W)')
+    xlabel('Time')
+    print -depsc gk2409_gd.eps;
+end
+
+function cost = get_cost(y,theta,kernel,lambda)
+
+    N=size(y,1);
+    total = 0;
+    for i=1:N
+        u = y(i)*theta'*kernel(:,i);
+        s =1./(1+exp(-u));
+        total = total + log(s);
+    end
+    
+    cost = -total./N + lambda*(theta'*theta);
+    
+end
+
+function g = ggradient(y, theta, lambda, kernel)
+    
+    N = size(y,1);
+    total=zeros(1,1000);
+    last = 2*lambda*theta';
+    for i=1:N
+        u = y(i)*theta'*kernel(:,i);
+        s = 1./(1+exp(-u));
+        ss = ((1-s)*y(i)*kernel(:,i)');
+        total = total + ss;
+    end
+    g = -total./N + last;
+    g=g';
+    
+end
+
+function t_error = error(theta,kernel,Y)
+    err = zeros(1000,1);
+    N = size(kernel,1);
+    for i=1:N
+        prob = 1./(1+ exp(-theta'*kernel(:,i)));
+        if (prob >=0.5)
+            err(i)=1;
+        else
+            err(i)=-1;
+        end
+    end
+    sum(err~=Y);
+    t_error = sum(err~=Y)/length(Y);
+end
+
+function err = errors(theta, gram_, y)
+    res = zeros(size(y));
+    for i=1:1000
+         f = 1./(1+exp(-theta'*gram_(:,i)));
+         if (f >= 0.5)
+            res(i) = 1;
+         else
+            res(i) = -1;
+         end
+    end
+    err = abs(sum(y==1) - sum(res==1)) + abs(sum(y==-1) - sum(res==-1));
+end
+
+function K = rbf_kernel(X)
 % Inputs:
 %       X:      data matrix with training samples in rows and features in columns
-%       X2:     data matrix with test samples in rows and features in columns
-%       sigma: width of the RBF kernel
 % Output:
 %       K: kernel matrix
 
     N = size(X,1);
-    for i=1:N
-        D(i,:) = pdist2(X(i,:),X2(i,:),'euclidean');
-        K(:,i) = exp(-D(i,:)/(1/(N^2)*(sum(D(i,:)))));
-    end
+    D=squareform(pdist(X,'euclidean')).^2;
+    K = exp(-D./(1/(N^2))*sum(D(:)));
 end
