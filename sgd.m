6df60d95cfec0df36a08a403881d6d8bb7e9abe5
Georgios Koloventzos <g.koloventzos@gmail.com>
2015-11-06 11:15:34 -0500

    last commit

--- sgd.m
+++ sgd.m
@@ -1,49 +1,37 @@
-function problem5(stepsize)
-
-gradient_decent(stepsize)
-
-end
-
-%function sgd(points)
-%
-%end
-
-function gradient_decent(stepsize)
+function sgd(stepsize, points)
     close all ; 
     load('data1.mat');
-    kernel = rbf_kernel(TrainingX);
     tol = exp(-3);
     lambda= exp(-2);
-    %prevtheta = 0.1 * (rand(size(TrainingX,1),1)-0.5);
+    kernel = rbf_kernel(TrainingX);
     prevtheta = ones(size(TrainingX,1),1);
+    theta = ones(size(TrainingX,1),1);
     curiter = 0;
-    %start time
+    shuffle=1:1:size(kernel,1);
+    shuffle=(randperm(size(kernel,1)));
+    points_for_gd = shuffle(1:points);
     time_start = tic;
     all_time(1) = time_start;
     cost(1) = get_cost(TrainingY,prevtheta,kernel,lambda);
-    S_G = ggradient(TrainingY, prevtheta, lambda, kernel);
-    theta = prevtheta - stepsize.*S_G;
-    diff = norm(theta - prevtheta);
-    while diff >= tol
-        % Update theta
+    %S_G = ggradient(TrainingY(points_for_gd(1:points)), prevtheta, lambda, kernel(points_for_gd(1:points)));
+    %theta = prevtheta - stepsize.*S_G;
+    %diff = norm(theta - prevtheta(points_for_gd(1:points)))+tol
+    diff=tol;
+    while diff >= tol 
         prevtheta = theta;
-        G = ggradient(TrainingY, prevtheta, lambda, kernel);
+        G = ggradient(TrainingY(points_for_gd(1:points)), prevtheta, lambda, kernel(points_for_gd(1:points),:));
         theta = prevtheta - stepsize.*G;
         time_now = toc(time_start);
         diff = norm(theta -prevtheta);
         curiter = curiter + 1;
         all_time(curiter) = time_now;
-        cost(curiter) = get_cost(TrainingY,prevtheta,kernel,lambda);
-        accuracy(curiter) = error(prevtheta,kernel,TestY);
+        cost(curiter) = get_cost(TrainingY,prevtheta(points_for_gd(1:points)),kernel,lambda);
+        accuracy(curiter) = error(prevtheta,kernel(points_for_gd(1:points)),TestY(points_for_gd(1:points)));
+        shuffle=1:1:size(kernel,1);
+        shuffle=(randperm(size(kernel,1)));
+        points_for_gd = shuffle(1:points);
     end
     fprintf ('Accuracy:%0.4f, step:%0.4f, iter:%d, total time: %d  \n' , accuracy(end), stepsize, curiter, all_time(end) );
-    figure(1)
-    %index = find(all_time > 20,1)
-    plot(all_time(:), cost(:))
-    name = sprintf('gk2409_gd_%4f.eps', stepsize);
-    ylabel('Cost -J(W)')
-    xlabel('Time')
-    print(name,'-depsc');
 end
 
 function cost = get_cost(y,theta,kernel,lambda)
@@ -52,6 +40,7 @@ function cost = get_cost(y,theta,kernel,lambda)
     total = 0;
     for i=1:N
         u = y(i)*theta'*kernel(:,i);
+        size(u);
         s =1./(1+exp(-u));
         total = total + log(s);
     end
@@ -65,10 +54,14 @@ function g = ggradient(y, theta, lambda, kernel)
     N = size(y,1);
     total=zeros(1,N);
     last = 2*lambda*theta';
+    size(kernel(:,1))
+    size(theta)
     for i=1:N
         u = y(i)*theta'*kernel(:,i);
         s = 1./(1+exp(-u));
         ss = ((1-s)*y(i)*kernel(:,i)');
+        size(total)
+        size(ss)
         total = total + ss;
     end
     g = -total./N + last;
@@ -77,10 +70,9 @@ function g = ggradient(y, theta, lambda, kernel)
 end
 
 function t_error = error(theta,kernel,Y)
-    
+
     N = size(kernel,1);
     err = zeros(N,1);
-    
     for i=1:N
         prob = 1./(1+ exp(-theta'*kernel(:,i)));
         if (prob >=0.5)
